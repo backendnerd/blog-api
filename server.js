41b1c49959c3533429d9de34a377dc58aa67418e
dawg <100376322+bitsflipp3r@users.noreply.github.com>
2022-11-29 20:20:27 +0100

    Error 404

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use("/api/v1/blog", blogRoutes);
 
 app.use("/", (req, res) => {
-    res.status(404).send("Error 404");
+    res.status(404).send("Opps... Something went wrong!<br>Error 404");
 });
 
 const server = app.listen(PORT, console.log(`Server running on port ${PORT}`));
